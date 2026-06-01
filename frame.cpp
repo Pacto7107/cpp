cc72ca21ce032ffdf40ee527aa1a068e4b30cd69
Pacto7107 <architectseoha5222@gmail.com>
2026-06-01 16:20:47 +0900

    Create frame.cpp

---
+++ frame.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#define fr(i,s,e) for(int (i) =(s); (i) < (e); (i)++)
+using ll = long long;
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+    ////////////
+
+    ////////////
+    return 0;
+}
